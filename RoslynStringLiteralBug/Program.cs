f0b58d93fd3460a8572eaf7cab36c4320ab1985d
Dan Ingalla <daningalla@gmail.com>
2020-06-18 18:26:10 -0600

    code

---
+++ RoslynStringLiteralBug/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace RoslynStringLiteralBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = string.Join(Environment.NewLine, new[]
+            {
+                "var list = new List<string>();",
+                "list.Add(\"found a bug?\");"
+            });
+            OutputTokens(code);
+            
+            code = string.Join(Environment.NewLine, new[]
+            {
+                "var list = new List<string>();",
+                "var bug = \"found a bug?\";",
+                "list.Add(bug);"
+            });
+            OutputTokens(code);
+        }
+
+        static void OutputTokens(string code)
+        {
+            Console.WriteLine($"Output tokens for:");
+            Console.WriteLine(code);
+            Console.WriteLine();
+            
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+            foreach (var token in syntaxTree.GetRoot().DescendantTokens())
+            {
+                Console.WriteLine($"{token.Kind()}: '{token}'");
+            }
+        }
+    }
+}
